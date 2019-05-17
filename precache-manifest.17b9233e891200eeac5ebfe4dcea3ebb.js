98794cf79867334cc11f4245d5cdc8c05e9b6f03
Clifton Noble <cliftonnoble@Cliftons-MacBook-Pro.local>
2019-05-16 21:08:34 -0700

    Updates

---
+++ precache-manifest.17b9233e891200eeac5ebfe4dcea3ebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bddd3af07e51930a23b2d61cbac135",
+    "url": "/warriors-memory-game/index.html"
+  },
+  {
+    "revision": "cfeb181a840444c6420b",
+    "url": "/warriors-memory-game/static/css/main.c508003d.chunk.css"
+  },
+  {
+    "revision": "1c70fd400692ec20cdb0",
+    "url": "/warriors-memory-game/static/js/2.1fea4741.chunk.js"
+  },
+  {
+    "revision": "cfeb181a840444c6420b",
+    "url": "/warriors-memory-game/static/js/main.7f471792.chunk.js"
+  },
+  {
+    "revision": "6933a8c308382a0f64d2",
+    "url": "/warriors-memory-game/static/js/runtime~main.c59e7830.js"
+  }
+]);
